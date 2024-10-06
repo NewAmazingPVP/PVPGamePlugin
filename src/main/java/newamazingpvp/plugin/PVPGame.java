e1a4ef83c990fffe1c3fbb2df788e1a3631147fe
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-10-06 00:32:57 -0400

    PVP Game server
    PVP Game server

---
+++ src/main/java/newamazingpvp/plugin/PVPGame.java
@@ -0,0 +1,17 @@
+package newamazingpvp.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PVPGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
