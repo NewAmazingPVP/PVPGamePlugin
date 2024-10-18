54a09e1862ee06d24899575475cf4546c0a09cc7
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-10-18 15:46:35 -0400

    1

--- src/main/java/newamazingpvp/plugin/Mystics/MysticWell.java
+++ src/main/java/newamazingpvp/plugin/Mystics/MysticWell.java
@@ -1,5 +1,7 @@
 package newamazingpvp.plugin.Mystics;
 
+
+import newamazingpvp.plugin.PVPGame;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -15,8 +17,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class MysticWell implements Listener{
+import static newamazingpvp.plugin.PVPGame.SERVER;
 
+public class MysticWell implements Listener {
 
 
     @EventHandler
@@ -45,72 +48,95 @@ public class MysticWell implements Listener{
 
     }
 
+    public static BukkitRunnable mysticWellIDLE;
 
     public static void openCustomWellMenu(Player player) {
 
         player.playSound(player.getLocation(), Sound.BLOCK_ENDER_CHEST_OPEN, 1.0f, 1.0f);
         Inventory mysticWell = Bukkit.createInventory(player, 45, "§d§l[Mystic Well]");
 
-        mysticWell.setItem(0,grayGlassGUI());
-        mysticWell.setItem(1,grayGlassGUI());
-        mysticWell.setItem(2,grayGlassGUI());
-        mysticWell.setItem(3,grayGlassGUI());
-        mysticWell.setItem(4,grayGlassGUI());
-        mysticWell.setItem(5,grayGlassGUI());
-        mysticWell.setItem(6,grayGlassGUI());
-        mysticWell.setItem(7,grayGlassGUI());
-        mysticWell.setItem(8,grayGlassGUI());
+        mysticWell.setItem(0, grayGlassGUI());
+        mysticWell.setItem(1, grayGlassGUI());
+        mysticWell.setItem(2, grayGlassGUI());
+        mysticWell.setItem(3, grayGlassGUI());
+        mysticWell.setItem(4, grayGlassGUI());
+        mysticWell.setItem(5, grayGlassGUI());
+        mysticWell.setItem(6, grayGlassGUI());
+        mysticWell.setItem(7, grayGlassGUI());
+        mysticWell.setItem(8, grayGlassGUI());
 
-        mysticWell.setItem(9,grayGlassGUI());
+        mysticWell.setItem(9, grayGlassGUI());
         //mysticWell.setItem(10,grayGlassGUI());
         //mysticWell.setItem(11,grayGlassGUI());
         //mysticWell.setItem(12,grayGlassGUI());
-        mysticWell.setItem(13,grayGlassGUI());
-        mysticWell.setItem(14,grayGlassGUI());
-        mysticWell.setItem(15,grayGlassGUI());
-        mysticWell.setItem(16,grayGlassGUI());
-        mysticWell.setItem(17,grayGlassGUI());
+        mysticWell.setItem(13, grayGlassGUI());
+        mysticWell.setItem(14, grayGlassGUI());
+        mysticWell.setItem(15, grayGlassGUI());
+        mysticWell.setItem(16, grayGlassGUI());
+        mysticWell.setItem(17, grayGlassGUI());
 
-        mysticWell.setItem(18,grayGlassGUI());
+        mysticWell.setItem(18, grayGlassGUI());
         //mysticWell.setItem(19,grayGlassGUI());
         //mysticWell.setItem(20,grayGlassGUI()); //enter slot
         //mysticWell.setItem(21,grayGlassGUI());
-        mysticWell.setItem(22,grayGlassGUI());
+        mysticWell.setItem(22, grayGlassGUI());
         //mysticWell.setItem(23,grayGlassGUI()); //sacrifice slot
-        mysticWell.setItem(24,grayGlassGUI());
+        mysticWell.setItem(24, grayGlassGUI());
         //mysticWell.setItem(25,grayGlassGUI()); // output slot
-        mysticWell.setItem(26,grayGlassGUI());
+        mysticWell.setItem(26, grayGlassGUI());
 
-        mysticWell.setItem(27,grayGlassGUI());
+        mysticWell.setItem(27, grayGlassGUI());
         //mysticWell.setItem(28,grayGlassGUI());
         //mysticWell.setItem(29,grayGlassGUI());
         //mysticWell.setItem(30,grayGlassGUI());
-        mysticWell.setItem(31,grayGlassGUI());
-        mysticWell.setItem(32,grayGlassGUI());
-        mysticWell.setItem(33,grayGlassGUI());
-        mysticWell.setItem(34,grayGlassGUI());
-        mysticWell.setItem(35,grayGlassGUI());
+        mysticWell.setItem(31, grayGlassGUI());
+        mysticWell.setItem(32, grayGlassGUI());
+        mysticWell.setItem(33, grayGlassGUI());
+        mysticWell.setItem(34, grayGlassGUI());
+        mysticWell.setItem(35, grayGlassGUI());
 
-        mysticWell.setItem(36,grayGlassGUI());
-        mysticWell.setItem(37,grayGlassGUI());
-        mysticWell.setItem(38,grayGlassGUI());
-        mysticWell.setItem(39,grayGlassGUI());
-        mysticWell.setItem(40,grayGlassGUI());
-        mysticWell.setItem(41,grayGlassGUI());
-        mysticWell.setItem(42,grayGlassGUI());
-        mysticWell.setItem(43,grayGlassGUI());
-        mysticWell.setItem(44,grayGlassGUI());
+        mysticWell.setItem(36, grayGlassGUI());
+        mysticWell.setItem(37, grayGlassGUI());
+        mysticWell.setItem(38, grayGlassGUI());
+        mysticWell.setItem(39, grayGlassGUI());
+        mysticWell.setItem(40, grayGlassGUI());
+        mysticWell.setItem(41, grayGlassGUI());
+        mysticWell.setItem(42, grayGlassGUI());
+        mysticWell.setItem(43, grayGlassGUI());
+        mysticWell.setItem(44, grayGlassGUI());
 
         player.openInventory(mysticWell);
 
 
+        mysticWellIDLE = new BukkitRunnable() {
+
+            @Override
+            public void run() {
+
+
+
+            }
+        };
+        mysticWellIDLE.runTaskTimer(PVPGame, 0L, 20L); // Start immediately and repeat every second
+
+
+
     }
+
+
+    //Animations
+
 
     public static void updateMysticWellMenuIDLE(int frame, Inventory inv) {
 
 
+
+
     }
 
+
+
+    //GUI Items
     public static ItemStack grayGlassGUI() {
         ItemStack ITEM = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
         ItemMeta META = ITEM.getItemMeta();
--- src/main/java/newamazingpvp/plugin/PVPGame.java
+++ src/main/java/newamazingpvp/plugin/PVPGame.java
@@ -3,12 +3,21 @@ package newamazingpvp.plugin;
 
 import newamazingpvp.plugin.Commands.AdminCMD.SetViewCMD;
 import newamazingpvp.plugin.Mystics.MysticWell;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PVPGame extends JavaPlugin {
 
+    public static PVPGame PVPGame;
+    public static Server SERVER;
+
     @Override
     public void onEnable() {
+
+        PVPGame = this;
+        SERVER = Bukkit.getServer();
+
 
         getServer().getPluginManager().registerEvents(new MysticWell(), this);
 
