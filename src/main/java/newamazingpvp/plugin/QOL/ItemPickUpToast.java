bd54b62ba42948af7ac954de71e22ce6465066fa
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-10-28 19:42:32 -0400

    1

---
+++ src/main/java/newamazingpvp/plugin/QOL/ItemPickUpToast.java
@@ -0,0 +1,28 @@
+package newamazingpvp.plugin.QOL;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class ItemPickUpToast implements Listener {
+
+    @EventHandler
+    public void playerPickUpItem(PlayerPickupItemEvent e){
+
+        Player player = e.getPlayer();
+        ItemStack item = e.getItem().getItemStack();
+        ItemMeta meta = item.getItemMeta();
+        String itemName = meta.getDisplayName();
+
+        player.sendTitle(" ", itemName, 10, 40, 5);
+
+
+
+    }
+
+}
