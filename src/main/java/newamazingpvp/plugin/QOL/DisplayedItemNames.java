0ae5a02a18705d7eccfb451c271a3f0df57e92ce
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-10-30 19:50:13 -0400

    1

--- src/main/java/newamazingpvp/plugin/QOL/DisplayedItemNames.java
+++ src/main/java/newamazingpvp/plugin/QOL/DisplayedItemNames.java
@@ -1,10 +1,9 @@
 package newamazingpvp.plugin.QOL;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -12,7 +11,7 @@ import java.util.Collection;
 
 import static newamazingpvp.plugin.PVPGame.world;
 
-public class DropedItemNames {
+public class DisplayedItemNames implements Listener {
 
     @EventHandler
     public void playerMove(PlayerMoveEvent e){
